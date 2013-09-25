9d6cb69f91284cfc69ee7c17e9ad726d3058e104
darkrodry <darkrodry@gmail.com>
2013-09-25 18:20:21 +0200

    change eyeD3 library to mutagen

--- src/songtageditor.py
+++ src/songtageditor.py
@@ -5,16 +5,23 @@
 __author__="DarkRodry"
 __date__ ="$25-sep-2013 16:55:48$"
 
-import eyed3, glob
-
+import glob
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, TIT2, TPE1, TRCK, TALB, USLT, error
+   
 def songEdit(songFileName):
-    songFile = eyed3.load(songFileName)
     artist = unicode(songFileName.split(' - ')[0].decode('utf8'))
     songTitle = unicode(songFileName.split(' - ')[1].split('.mp3')[0].decode('utf8'))
-    songFile.tag.artist = artist
-    songFile.tag.title = songTitle
-    songFile.tag.album = u" "
-    songFile.tag.save()
+    audio = MP3(songFileName, ID3=ID3)
+    try:
+        audio.add_tags()
+    except:
+        pass
+    audio.tags.add(TIT2(encoding=3, text=songTitle))
+    audio.tags.add(TALB(encoding=3, text="".decode('utf-8')))
+    audio.tags.add(TPE1(encoding=3, text=artist))
+    audio.tags.add(TRCK(encoding=3, text="".decode('utf-8')))
+    audio.save()
     
 if __name__ == "__main__":
     songList = glob.glob("*.mp3")
